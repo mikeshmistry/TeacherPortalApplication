10939f7fc4a49f6f726b8741264f59b0d5f56d0d
Happy user <Happy user@DESKTOP-C8BUOD4>
2021-01-11 21:56:33 -0500

    Remove classes

--- TeacherPortal.BL/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TeacherPortal.BL
-{
-    public class Class1
-    {
-    }
-}
--- TeacherPortal.UI.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TeacherPortal.UI.Models
-{
-    public class Class1
-    {
-    }
-}
