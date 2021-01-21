8b1e57f2af957afd9cd76b16f30fae51934663b0
Happy user <Happy user@DESKTOP-C8BUOD4>
2021-01-20 23:02:16 -0500

    Added Controllers

--- TeacherPortal/Controllers/HomeController.cs
+++ TeacherPortal/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
+using TeacherPortal.UI.Models.SecurityModels;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using TeacherPortal.Models;
 
 namespace TeacherPortal.Controllers
 {
---
+++ TeacherPortal/Controllers/MainController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentPortal.UI.Controllers
+{
+    /// <summary>
+    /// Controller For the Logged in teacher
+    /// </summary>
+    [AutoValidateAntiforgeryToken]
+    [Authorize]
+    public class MainController : Controller
+    {
+
+        #region Action Methods
+
+        /// <summary>
+        /// Get action method for main
+        /// </summary>
+        /// <returns>The main view for the logged in user</returns>
+        public IActionResult Index()
+        {
+            return View("Views/Main.cshtml");
+           
+        }
+
+        #endregion
+    }
+}
