2710464e8aad41933baf2ce867ae5af5a6c70a34
Happy user <Happy user@DESKTOP-C8BUOD4>
2021-01-20 23:07:25 -0500

    Added Context Classes

---
+++ TeacherPortal.Security/Application/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TeacherPortal.Security.Application
+{
+
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            //insure that the database is created
+            base.Database.Migrate();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            //Change the table names
+            builder.Entity<ApplicationUser>().ToTable("User");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogin");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserToken");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaim");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRole");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaim");
+            builder.Entity<IdentityRole>().ToTable("Role");
+        }
+
+
+
+
+
+    }
+
+    }
+
---
+++ TeacherPortal.Security/Application/ApplicationUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeacherPortal.Security.Application
+{
+    /// <summary>
+    /// Class to represent an user
+    /// </summary>
+    public class ApplicationUser : IdentityUser
+    {
+
+        #region Properties
+        
+        /// <summary>
+        /// The teacher Id of the teacher
+        /// </summary>
+        [Required]
+        public int TeacherId { get; set; } 
+
+        #endregion 
+
+    }
+}
