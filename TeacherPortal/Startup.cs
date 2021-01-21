c650df01d0250bbb6bb9acec887df0899e505cf2
Happy user <Happy user@DESKTOP-C8BUOD4>
2021-01-20 23:06:19 -0500

    Added Database Connections

--- TeacherPortal/Startup.cs
+++ TeacherPortal/Startup.cs
@@ -1,40 +1,73 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using TeacherPortal.Data;
+using TeacherPortal.Security;
+using TeacherPortal.Security.Application;
 
 namespace TeacherPortal
 {
+    /// <summary>
+    /// Setup Class for the ASP.NET MVC Core application
+    /// </summary>
     public class Startup
     {
+        #region Property
+
+        /// <summary>
+        /// Property for the configuration object
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        #endregion
+
+        #region Constructor
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        #endregion
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Add the context for the security tables
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    Configuration.GetConnectionString("TeacherPortal")));
+
+            //Add the Application User
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
+
+            services.AddMvc();
             services.AddRazorPages();
+
+
+            //Configure the Password options
+            services.Configure<IdentityOptions>(ConfigurationOptions.PasswordOptions);
+
+            //Configure the Password options
+            services.Configure<IdentityOptions>(ConfigurationOptions.LockOutOptions);
+
+            //Configure the User options
+            services.Configure<IdentityOptions>(ConfigurationOptions.UserOptions);
+
+            //Configure cooking options
+            services.ConfigureApplicationCookie(ConfigurationOptions.CookieOptions);
+
         }
+
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
